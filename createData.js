6a9d1897e02cb446614a0d06b1d6f9f6ea5c519d
Fathoni Zikri Nugroho <fathonizikri11@gmail.com>
2022-12-04 16:41:16 +0700

    add create data

---
+++ createData.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/db-mongoose", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String,
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+// const apple = new Fruit({
+//   name: "apple",
+//   rating: 8,
+//   review: "Rasanya manis",
+// });
+const pepaya = new Fruit({
+  name: "pepaya",
+  rating: 10,
+  review: "Rasanya manis",
+});
+const pisang = new Fruit({
+  name: "pisang",
+  rating: 9,
+  review: "Aku suka pisang",
+});
+const mangga = new Fruit({
+  name: "mangga",
+  rating: 9,
+  review: "kita suka mangga",
+});
+
+// apple.save(function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("berhasil simpan data buah baru");
+//   }
+// });
+
+Fruit.insertMany([pepaya, pisang, mangga], function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("berhasil simpan data buah baru");
+  }
+});
